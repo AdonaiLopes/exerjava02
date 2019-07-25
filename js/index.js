4222f0e0a779637562602e1be833cf06a8ba0cf4
Adonai <adonailopes@hotmail.com>
2019-07-24 21:28:55 -0300

    aula javascript 02

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+var age= parseInt(prompt("digite numero"));
+if(age>=65) {
+	document.body.append("você é idoso");
+}
+else {
+	if(age>=18) {
+		document.body.append("você é maior");
+	} else {
+	if(age<=12) {
+		document.body.append("voce é crianca");}
+	} else {
+		document.body.append("voce é adolescente");
+	}
+}
+	
+
+
+
+
+
+
+
